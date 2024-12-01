60c2d9fc1d24b0837438d5de5b3fa632075acafe
sanketbutle10 <sanketbutle10@gmail.com>
2024-12-01 14:15:02 +0530

    Demo2 created

---
+++ src/Sample1/Demo2.java
@@ -0,0 +1,19 @@
+package Sample1;
+
+public class Demo2 {
+	
+	public static void main(String[] args) {
+		
+		m1();
+		m2();
+	}
+	
+	public static void m1() {
+		System.out.println("A1");
+	}
+	
+	public static void m2() {
+		System.out.println("A2");
+	}
+
+}
