1b8c2f5c83296dc47d259f18f8abc65bf1b1fa81
Sanket <sanket.butle@bonamisoftware.com>
2026-05-12 21:40:08 +0530

    commit

--- src/Sample1/Demo1.java
+++ src/Sample1/Demo1.java
@@ -3,13 +3,10 @@ package Sample1;
 public class Demo1 {
 	
 	public static void main(String[] args) {
-		
-<<<<<<< HEAD
-		System.out.println("abcd");
-=======
+
 		System.out.println("abcdd");
 		System.out.println("1234");
->>>>>>> refs/remotes/origin/master
+
 	}
 
 }
