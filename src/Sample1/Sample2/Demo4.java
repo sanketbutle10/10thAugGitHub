f8c75bad1bde76e4ebed1404c253e6e6dd27d008
Sanket <sanket.butle@bonamisoftware.com>
2026-05-15 14:52:25 +0530

    commiting d4

---
+++ src/Sample1/Sample2/Demo4.java
@@ -0,0 +1,12 @@
+public class Demo4 {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("abcd");
+        System.out.println("wxyz");
+
+        
+
+    }
+
+}
